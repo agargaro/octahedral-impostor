59f7a4999ea7b06840dcb163c28caa3392eef883
ANDREA GARGARO <devgargaro@gmail.com>
2025-04-07 18:09:40 +0200

    w

--- examples/example.ts
+++ examples/example.ts
@@ -16,7 +16,7 @@ const mesh = gltf.scene;
 
 scene.add(mesh, new HemisphereLight('white', 'green'), new AmbientLight());
 
-const countPerSide = 6;
+const countPerSide = 8;
 
 const albedoRenderTarget = createAlbedo({
   renderer: main.renderer,
@@ -59,18 +59,14 @@ planeSprite.material.onBeforeCompile = (p, renderer) => {
   p.uniforms.depthMap = { value: testRT };
 
   p.vertexShader = p.vertexShader.replace('void main() {', `
-    attribute vec3 tangent;
+      attribute vec3 tangent;
       uniform float countPerSide;
       varying vec4 framesWeight;
       flat varying vec2 frame1;
       flat varying vec2 frame2;
       flat varying vec2 frame3;  
-      varying vec3 TangentFragPos1;
-      varying vec3 TangentViewPos1;
-      varying vec3 TangentFragPos2;
-      varying vec3 TangentViewPos2;
-      varying vec3 TangentFragPos3;
-      varying vec3 TangentViewPos3;
+      varying vec3 TangentFragPos;
+      varying vec3 TangentViewPos;
 
       vec2 hemiOctaDirToGrid(vec3 dir) {
         vec3 octant = sign(dir);
@@ -84,14 +80,6 @@ planeSprite.material.onBeforeCompile = (p, renderer) => {
         position.y = 1. - abs(position.x) - abs(position.z);
         return position;
       }
-
-      mat3 lookAt(vec3 eye) {
-          vec3 z = normalize(eye);
-          vec3 up = vec3(0, 1, 0);
-          vec3 x = normalize(cross(up, z));
-          vec3 y = cross(z, x);
-          return mat3(x, y, z);
-      }
       
       vec4 computeFramesWeight(vec2 coords) {
           float a = min(1. - coords.x, 1. - coords.y);
@@ -99,6 +87,20 @@ planeSprite.material.onBeforeCompile = (p, renderer) => {
           float c = min(coords.x, coords.y);
           float d = ceil(coords.x - coords.y);
           return vec4(a, b, c, d);
+      }
+
+      vec3 orientPlaneToDirection(vec3 spriteNormal) {
+        vec3 z = normalize(spriteNormal);
+
+        vec3 up = vec3(0, 1, 0);
+        if (abs(z.y) > 0.99) {
+            up = vec3(1, 0, 0);
+        }
+
+        vec3 x = normalize(cross(up, z));
+        vec3 y = normalize(cross(z, x));
+
+        return x * position.x + y * position.y;
       }
 
       mat3 getTBN(vec3 normal) {
@@ -129,34 +131,14 @@ planeSprite.material.onBeforeCompile = (p, renderer) => {
           frame3 = clamp(frame1 + vec2(1), vec2(0,0), framesMinusOne);
 
           vec3 spriteNormal1 = hemiOctaGridToDir(frame1 * tileSize);
-          vec3 spriteNormal2 = hemiOctaGridToDir(frame2 * tileSize);
-          vec3 spriteNormal3 = hemiOctaGridToDir(frame3 * tileSize);
-          vec3 spriteNormal = normalize(spriteNormal1 * framesWeight.x + spriteNormal2 * framesWeight.y + spriteNormal3 * framesWeight.z);
     `);
 
   p.vertexShader = p.vertexShader.replace('#include <project_vertex>', `
-      mat3 axisLockMatrix = inverse(lookAt(spriteNormal));
-      transformed *= axisLockMatrix; 
-
-      vec3 posWorld = (modelMatrix * vec4(transformed, 1.0)).xyz;
+      transformed = orientPlaneToDirection(spriteNormal1);
 
       mat3 TBN1 = getTBN(spriteNormal1);
-      mat3 axisLockMatrix1 = inverse(lookAt(spriteNormal1));
-      vec3 posWorld1 = (modelMatrix * vec4(position * axisLockMatrix1, 1.0)).xyz;
-      TangentViewPos1  = TBN1 * cameraPosition;
-      TangentFragPos1  = TBN1 * posWorld1;
-
-      mat3 TBN2 = getTBN(spriteNormal2);
-      mat3 axisLockMatrix2 = inverse(lookAt(spriteNormal2));
-      vec3 posWorld2 = (modelMatrix * vec4(position * axisLockMatrix2, 1.0)).xyz;
-      TangentViewPos2  = TBN2 * cameraPosition;
-      TangentFragPos2  = TBN2 * posWorld2;
-
-      mat3 TBN3 = getTBN(spriteNormal3);
-      mat3 axisLockMatrix3 = inverse(lookAt(spriteNormal3));
-      vec3 posWorld3 = (modelMatrix * vec4(position * axisLockMatrix3, 1.0)).xyz;
-      TangentViewPos3  = TBN3 * cameraPosition;
-      TangentFragPos3  = TBN3 * posWorld3;
+      TangentViewPos  = TBN1 * cameraPosition;
+      TangentFragPos  = TBN1 * transformed;
 
       #include <project_vertex>
     `);
@@ -167,18 +149,14 @@ planeSprite.material.onBeforeCompile = (p, renderer) => {
       flat varying vec2 frame1;
       flat varying vec2 frame2;
       flat varying vec2 frame3;
-      varying vec3 TangentFragPos1;
-      varying vec3 TangentViewPos1;
-      varying vec3 TangentFragPos2;
-      varying vec3 TangentViewPos2;
-      varying vec3 TangentFragPos3;
-      varying vec3 TangentViewPos3;
+      varying vec3 TangentFragPos;
+      varying vec3 TangentViewPos;
 
       uniform sampler2D depthMap;
 
       vec2 parallaxMapping(vec2 uv, vec2 frame, vec3 tangentViewPos, vec3 tangentFragPos) {
         float tileSize = 1. / countPerSide; // TODO param
-        float heightScale = 0.03; // TODO
+        float heightScale = 0.0; // TODO
 
         vec3 viewDir = normalize(tangentViewPos - tangentFragPos);
 
@@ -192,18 +170,18 @@ planeSprite.material.onBeforeCompile = (p, renderer) => {
 
   p.fragmentShader = p.fragmentShader.replace('#include <map_fragment>', `
       float tileSize = 1. / countPerSide;
-      
-      vec2 uv1 = parallaxMapping(vMapUv, frame1, TangentViewPos1, TangentFragPos1);
-      vec2 uv2 = parallaxMapping(vMapUv, frame2, TangentViewPos2, TangentFragPos2);
-      vec2 uv3 = parallaxMapping(vMapUv, frame3, TangentViewPos3, TangentFragPos3);
 
-      // if (uv.x < 0.0 || uv.x > 1.0 || uv.y < 0.0 || uv.y > 1.0) discard;
+      vec2 uv = parallaxMapping(vMapUv, frame1, TangentViewPos, TangentFragPos);
 
-      vec4 quad_a = texture2D(map, (uv1 + frame1) * tileSize);
-      vec4 quad_b = texture2D(map, (uv2 + frame2) * tileSize);
-      vec4 quad_c = texture2D(map, (uv3 + frame3) * tileSize);
+      if (uv.x < 0.0 || uv.x > 1.0 || uv.y < 0.0 || uv.y > 1.0) discard;
+      vec4 quad_a = texture2D(map, (uv + frame1) * tileSize);
+
+      // vec4 quad_a = texture2D(map, (vMapUv + frame1) * tileSize);
+      // vec4 quad_b = texture2D(map, (vMapUv + frame2) * tileSize);
+      // vec4 quad_c = texture2D(map, (vMapUv + frame3) * tileSize);
       
-      vec4 blendedColor = quad_a * framesWeight.x + quad_b * framesWeight.y + quad_c * framesWeight.z;
+      // vec4 blendedColor = quad_a * framesWeight.x + quad_b * framesWeight.y + quad_c * framesWeight.z;
+      vec4 blendedColor = quad_a;
 
       if (blendedColor.a == 0.0) discard;
       diffuseColor *= blendedColor;
