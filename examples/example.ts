62d69b07f90fccacdd09bf14bac57df6ec193b35
ANDREA GARGARO <devgargaro@gmail.com>
2025-04-15 22:45:29 +0200

    wip

--- examples/example.ts
+++ examples/example.ts
@@ -2,9 +2,8 @@ import { Asset, Main, OrthographicCameraAuto } from '@three.ez/main';
 import { AmbientLight, HemisphereLight, Scene } from 'three';
 import { GLTF, GLTFLoader, OrbitControls } from 'three/examples/jsm/Addons.js';
 import GUI from 'three/examples/jsm/libs/lil-gui.module.min.js';
-import { exportTextureFromRenderTarget, OctahedronImpostor, OctahedronImpostorMaterialGenerator } from '../src/index.js';
+import { OctahedronImpostor, OctahedronImpostorMaterialGenerator } from '../src/index.js';
 
-// const mainCamera = new OrthographicCameraAuto(20).translateZ(100).translateY(100).translateX(100);
 const mainCamera = new OrthographicCameraAuto(20).translateZ(100);
 const scene = new Scene();
 const main = new Main(); // init renderer and other stuff
@@ -12,7 +11,7 @@ const controls = new OrbitControls(mainCamera, main.renderer.domElement);
 controls.maxPolarAngle = Math.PI / 2;
 controls.update();
 
-Asset.load<GLTF>(GLTFLoader, 'cliff.gltf').then((gltf) => {
+Asset.load<GLTF>(GLTFLoader, 'palm.gltf').then((gltf) => {
   const mesh = gltf.scene;
 
   scene.add(mesh, new HemisphereLight('white', 'green'), new AmbientLight());
@@ -31,11 +30,10 @@ Asset.load<GLTF>(GLTFLoader, 'cliff.gltf').then((gltf) => {
   // exportTextureFromRenderTarget(main.renderer, materialRT._albedoRT, 'normal');
   // exportTextureFromRenderTarget(main.renderer, materialRT._depthMapRT, 'depth');
 
-  const impostor = new OctahedronImpostor(material).translateY(0.52).translateX(0.07);
-  impostor.scale.multiplyScalar(2);
+  const impostor = new OctahedronImpostor(material).translateX(0.19).translateY(0.35);
   scene.add(impostor);
 
-  mesh.scale.divideScalar(4.8);
+  mesh.scale.divideScalar(3.85);
   mesh.visible = false;
 
   main.createView({ scene, camera: mainCamera, backgroundColor: 'cyan' });
