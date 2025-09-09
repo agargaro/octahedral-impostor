283ba8c50665e8f731457e867b25c88c9e2aebe1
agargaro <devgargaro@gmail.com>
2025-09-09 21:53:36 +0200

    remove antialiasing and better material handling

--- examples/showcase/world.ts
+++ examples/showcase/world.ts
@@ -1,20 +1,15 @@
 import { createRadixSort, InstancedMesh2 } from '@three.ez/instanced-mesh';
 import { Asset, Main, PerspectiveCameraAuto } from '@three.ez/main';
 import { simplifyGeometriesByError } from '@three.ez/simplify-geometry';
-import { ACESFilmicToneMapping, AmbientLight, DirectionalLight, EquirectangularReflectionMapping, FogExp2, HalfFloatType, Material, Mesh, MeshLambertMaterial, MeshStandardMaterial, RepeatWrapping, Scene, Texture, TextureLoader } from 'three';
-import 'three-hex-tiling';
-import { GLTF, GLTFLoader, MapControls, RGBELoader } from 'three/examples/jsm/Addons.js';
+import { AmbientLight, BufferGeometry, Color, DirectionalLight, FogExp2, Material, Mesh, MeshLambertMaterial, MeshStandardMaterial, RepeatWrapping, Scene, Texture, TextureLoader } from 'three';
+import { GLTF, GLTFLoader, MapControls } from 'three/examples/jsm/Addons.js';
 import { mergeGeometries } from 'three/examples/jsm/utils/BufferGeometryUtils.js';
 import { OctahedralImpostor } from '../../src/core/octahedralImpostor.js';
 import { Terrain, TerrainParams } from './terrain.js';
 
-// TODO: render terrain first to avoid impostor overdraw
-// TODO: LOD before impostor
-// TODO: BVH chunk
-
-const camera = new PerspectiveCameraAuto(50, 0.1, 5000).translateY(50);
+const camera = new PerspectiveCameraAuto(50, 0.1, 1500).translateY(50);
 const scene = new Scene();
-const main = new Main({ showStats: false }); // init renderer and other stuff
+const main = new Main({ showStats: true, rendererParameters: { antialias: false } }); // init renderer and other stuff
 
 const controls = new MapControls(camera, main.renderer.domElement);
 controls.maxPolarAngle = Math.PI / 2;
@@ -23,35 +18,23 @@ controls.update();
 
 main.renderer.setPixelRatio(Math.min(1.5, window.devicePixelRatio)); // TODO mmm...
 
-// main.renderer.toneMapping = ACESFilmicToneMapping;
-// main.renderer.toneMappingExposure = 0.6;
-
 Asset.load<GLTF>(GLTFLoader, 'tree.glb').then(async (gltf) => {
   const mesh = gltf.scene;
 
-  const loader = new RGBELoader();
-  loader.type = HalfFloatType;
-  loader.load(`skybox3.hdr`, function (texture) {
-    texture.mapping = EquirectangularReflectionMapping;
-    scene.background = texture;
-  });
+  scene.background = new Color('cyan');
 
   const directionalLight = new DirectionalLight('white', 2);
   const ambientLight = new AmbientLight('white', 1);
 
   scene.add(directionalLight, ambientLight);
 
-  scene.fog = new FogExp2(0xbfc3d0, 0.001);
+  scene.fog = new FogExp2('cyan', 0.0012);
 
   // TERRAIN
 
-  const grassNormalMap = await Asset.load<Texture>(TextureLoader, 'grass_normal.png');
-  grassNormalMap.wrapS = grassNormalMap.wrapT = RepeatWrapping;
-  grassNormalMap.repeat.set(100, 100);
-
   const grassMap = await Asset.load<Texture>(TextureLoader, 'grass.jpg');
   grassMap.wrapS = grassMap.wrapT = RepeatWrapping;
-  grassMap.repeat.set(100, 100);
+  grassMap.repeat.set(50, 50);
 
   const options: TerrainParams = {
     maxChunksX: 24,
@@ -65,8 +48,8 @@ Asset.load<GLTF>(GLTFLoader, 'tree.glb').then(async (gltf) => {
     gain: 0.2
   };
 
-  const terrain = new Terrain(new MeshStandardMaterial({ color: 0xdddddd, map: grassMap, normalMap: grassNormalMap, hexTiling: {} }), options);
-  terrain.receiveShadow = true;
+  const terrain = new Terrain(new MeshLambertMaterial({ color: 0xaaaaaa, map: grassMap }), options);
+  terrain.renderOrder = 1;
 
   for (let x = -(options.maxChunksX / 2); x < (options.maxChunksX / 2); x++) {
     for (let z = -(options.maxChunksZ / 2); z < (options.maxChunksZ / 2); z++) {
@@ -77,9 +60,14 @@ Asset.load<GLTF>(GLTFLoader, 'tree.glb').then(async (gltf) => {
 
   // TREES AND IMPOSTORS
 
-  const oldMaterial = mesh.children[0].material as MeshStandardMaterial;
-  mesh.children[0].material = new MeshLambertMaterial({ alphaTest: 0.4, map: oldMaterial.map });
-  mesh.children[0].material.map.generateMipmaps = false;
+  const leavesMesh = mesh.children[0] as Mesh<BufferGeometry, MeshLambertMaterial>;
+  const oldLeaveesMaterial = leavesMesh.material;
+  leavesMesh.material = new MeshLambertMaterial({ alphaTest: 0.4, map: oldLeaveesMaterial.map });
+  leavesMesh.material.map.generateMipmaps = false;
+
+  const trunkMesh = mesh.children[1] as Mesh<BufferGeometry, MeshLambertMaterial>;
+  const oldTrunkMaterial = trunkMesh.material;
+  trunkMesh.material = new MeshLambertMaterial({ map: oldTrunkMaterial.map });
 
   const mergedGeo = mergeGeometries(mesh.children.map((x) => (x as Mesh).geometry), true);
   const materials = mesh.children.map((x) => (x as Mesh).material as Material);
@@ -89,7 +77,7 @@ Asset.load<GLTF>(GLTFLoader, 'tree.glb').then(async (gltf) => {
   const iMesh = new InstancedMesh2(mergedGeo, materials, { createEntities: true, renderer: main.renderer, capacity: pos.length });
 
   // iMesh.sortObjects = true;
-  iMesh.customSort = createRadixSort(iMesh);
+  // iMesh.customSort = createRadixSort(iMesh);
 
   iMesh.addInstances(pos.length, (obj, index) => {
     obj.position.copy(pos[index]);
