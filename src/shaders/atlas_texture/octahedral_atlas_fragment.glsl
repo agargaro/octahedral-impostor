c3c63e8c2d892de906783202cb16c9f26723667a
agargaro <devgargaro@gmail.com>
2025-11-02 10:40:37 +0100

    Improve backing shader

--- src/shaders/atlas_texture/octahedral_atlas_fragment.glsl
+++ src/shaders/atlas_texture/octahedral_atlas_fragment.glsl
@@ -4,26 +4,17 @@ uniform float opacity;
 #if defined( FLAT_SHADED ) || defined( USE_BUMPMAP ) || defined( USE_NORMALMAP_TANGENTSPACE )
 	varying vec3 vViewPosition;
 #endif
-// #include <packing>
+#include <packing>
 #include <normal_pars_fragment>
-// #include <bumpmap_pars_fragment>
+#include <bumpmap_pars_fragment>
 #include <normalmap_pars_fragment>
 #include <common>
-// #include <dithering_pars_fragment>
 #include <color_pars_fragment>
 #include <uv_pars_fragment>
 #include <map_pars_fragment>
 #include <alphamap_pars_fragment>
 #include <alphatest_pars_fragment>
 #include <alphahash_pars_fragment>
-// #include <aomap_pars_fragment>
-// #include <lightmap_pars_fragment>
-// // #include <envmap_common_pars_fragment>
-// // #include <envmap_pars_fragment>
-// // #include <fog_pars_fragment>
-// // #include <specularmap_pars_fragment>
-// // #include <logdepthbuf_pars_fragment>
-// #include <clipping_planes_pars_fragment>
 varying vec2 vHighPrecisionZW;
 
 layout(location = 0) out vec4 gAlbedo;
@@ -31,8 +22,6 @@ layout(location = 1) out vec4 gNormalDepth;
 
 void main() {
 	vec4 diffuseColor = vec4( diffuse, opacity );
-	// #include <clipping_planes_fragment>
-	// #include <logdepthbuf_fragment>
     #include <normal_fragment_begin>
 	#include <normal_fragment_maps>
 	#include <map_fragment>
@@ -40,18 +29,6 @@ void main() {
 	#include <alphamap_fragment>
 	#include <alphatest_fragment>
 	#include <alphahash_fragment>
-	// // #include <specularmap_fragment>
-	// ReflectedLight reflectedLight = ReflectedLight( vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ) );
-	// #ifdef USE_LIGHTMAP
-	// 	vec4 lightMapTexel = texture2D( lightMap, vLightMapUv );
-	// 	reflectedLight.indirectDiffuse += lightMapTexel.rgb * lightMapIntensity * RECIPROCAL_PI;
-	// #else
-	// 	reflectedLight.indirectDiffuse += vec3( 1.0 );
-	// #endif
-	// #include <aomap_fragment>
-	// reflectedLight.indirectDiffuse *= diffuseColor.rgb;
-	// vec3 outgoingLight = reflectedLight.indirectDiffuse;
-	// // #include <envmap_fragment>
 
 	// // #include <opaque_fragment>
     #ifdef OPAQUE
@@ -61,33 +38,20 @@ void main() {
 	diffuseColor.a *= material.transmissionAlpha;
 	#endif
 	gAlbedo = diffuseColor;
-	// gAlbedo = vec4( outgoingLight, diffuseColor.a );
 
-	// // #include <tonemapping_fragment>
-    #if defined( TONE_MAPPING )
-		gAlbedo.rgb = toneMapping( gl_FragColor.rgb );
-	#endif
+	// // // #include <tonemapping_fragment>
+    // #if defined( TONE_MAPPING )
+	// 	gAlbedo.rgb = toneMapping( gl_FragColor.rgb );
+	// #endif
 
 	// // #include <colorspace_fragment>
 	gAlbedo = linearToOutputTexel( gAlbedo );
 
-	// // #include <fog_fragment>
 	// // #include <premultiplied_alpha_fragment>
     #ifdef PREMULTIPLIED_ALPHA
 		gAlbedo.rgb *= gAlbedo.a;
 	#endif
 
-	// // #include <dithering_fragment>
-    #ifdef DITHERING
-		gAlbedo.rgb = dithering( gAlbedo.rgb );
-	#endif
-
-    // // #ifdef USE_REVERSED_DEPTH_BUFFER
-	// // 	float fragCoordZ = vHighPrecisionZW[ 0 ] / vHighPrecisionZW[ 1 ];
-	// // #else
 	float fragCoordZ = 0.5 * vHighPrecisionZW[ 0 ] / vHighPrecisionZW[ 1 ] + 0.5;
-	// // #endif
-
-    gNormalDepth = vec4( normal, 1.0 - fragCoordZ );
-    // // gNormalDepth = vec4( packNormalToRGB( normal ), 1.0 - fragCoordZ );
+    gNormalDepth = vec4( packNormalToRGB( normal ), 1.0 - fragCoordZ );
 }
--- src/shaders/atlas_texture/octahedral_atlas_vertex.glsl
+++ src/shaders/atlas_texture/octahedral_atlas_vertex.glsl
@@ -4,22 +4,19 @@
 	varying vec3 vViewPosition;
 #endif
 #include <common>
-// #include <batching_pars_vertex>
+#include <batching_pars_vertex>
 #include <uv_pars_vertex>
-// #include <displacementmap_pars_vertex> 
+#include <displacementmap_pars_vertex> 
 #include <normal_pars_vertex> 
-// // #include <envmap_pars_vertex>
 #include <color_pars_vertex>
-// // #include <fog_pars_vertex>
 // #include <morphtarget_pars_vertex>
 // #include <skinning_pars_vertex>
-// // #include <logdepthbuf_pars_vertex>
-// #include <clipping_planes_pars_vertex>
 varying vec2 vHighPrecisionZW;
+
 void main() {
 	#include <uv_vertex>
 	#include <color_vertex>
-//     #include <batching_vertex>
+	#include <batching_vertex>
 	#include <beginnormal_vertex>
 // 	#include <morphinstance_vertex>
 // 	#include <morphcolor_vertex>
@@ -31,12 +28,7 @@ void main() {
 	#include <begin_vertex>
 // 	#include <morphtarget_vertex>
 // 	#include <skinning_vertex>
-//     #include <displacementmap_vertex>
+    #include <displacementmap_vertex>
 	#include <project_vertex>
-// 	// #include <logdepthbuf_vertex>
-// 	#include <clipping_planes_vertex>
     vHighPrecisionZW = gl_Position.zw;
-// 	#include <worldpos_vertex>
-// 	// #include <envmap_vertex>
-//     // #include <fog_vertex>
 }
